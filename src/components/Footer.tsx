7e80d539bf935e41c39bf343c061510051a22741
Rigel Gregory <rigel.rahateen@student.president.ac.id>
2025-04-29 09:06:46 +0700

    feat: added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="bg-accent/25 border-t-2 h-[100px] flex items-center justify-center text-sm text-foreground/50">
+            <p>Made by <a href="https://github.com/rigelgfr" className="hover:underline" target="_blank" rel="noreferrer"> @rigelgfr</a></p>
+        </div>
+    )
+}
